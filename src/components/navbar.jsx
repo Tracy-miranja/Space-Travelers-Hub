2ef377423fe10d33d1a9f8e401f1b34fba1fc599
Samson Tush <samo4421@icloud.com>
2023-04-17 19:22:31 +0300

    Remove collapse from navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,13 +2,10 @@ import { Link } from 'react-router-dom';
 
 const NavBar = () => (
   <>
-    <nav className="navbar navbar-expand-lg bg-body-tertiary ps-4 pe-4">
+    <nav className="navbar navbar-expand ps-4 pe-4">
       <div className="container-fluid">
         <Link className="navbar-brand" to="/">Navbar</Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon" />
-        </button>
-        <div className="collapse navbar-collapse d-flex justify-content-end" id="navbarNav">
+        <div className="navbar-collapse d-flex justify-content-end" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
               <Link className="nav-link active" aria-current="page" to="/">Rockets</Link>
--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -1,6 +1,8 @@
 const Profile = () => (
   <>
-    <h1>Profile</h1>
+    <section className="mt-5 bg-primary">
+      <h1>Profile</h1>
+    </section>
   </>
 );
 
