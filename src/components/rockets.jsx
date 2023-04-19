a67a9219a0c44516e5b496f8a49299a390eacf00
Samson Tush <samo4421@icloud.com>
2023-04-19 13:24:04 +0300

    Add lists render

--- src/components/rockets.jsx
+++ src/components/rockets.jsx
@@ -1,6 +1,7 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { getRocket } from '../redux/Rocket/RocketsSlice';
+import './rockets.scss';
 
 const Rockets = () => {
   const { rockets, loading } = useSelector((state) => state.rockets);
@@ -16,14 +17,26 @@ const Rockets = () => {
 
   return (
     <>
-      <h1>Rockets</h1>
-      {rockets.map((item) => (
-        <div key={item.id}>
-          <h4>{item.name}</h4>
-          <p>{item.description}</p>
-          <img src={item.flickr_images[0]} alt="rocket" />
-        </div>
-      ))}
+      <section>
+        <hr />
+        {rockets.map((item) => (
+          <div key={item.id} className="container mt-4">
+            <div className="row">
+              <div className="col col-3 image">
+                <img src={item.flickr_images[0]} alt="rocket" />
+              </div>
+              <div className="col col-9 mt-2">
+                <h4>{item.name}</h4>
+                <p>
+                  <button type="submit" className="btn reserved me-2">Reserved</button>
+                  {item.description}
+                </p>
+                <button type="submit" className="btn btn-primary btn-lg">Reserve Rocket</button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </section>
     </>
   );
 };
